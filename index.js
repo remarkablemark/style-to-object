24f4f0211ac2ad2f429fa14d5ab203779ec4d6e7
Mark <mark@remarkablemark.org>
2017-10-20 00:05:56 -0400

    feat(parser): create parser that returns null for invalid values

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Parses inline style to object.
+ *
+ * @param {String} style
+ * @return {Object|null}
+ */
+module.exports = function parseInlineStyle(style) {
+  if (!style || typeof style !== 'string') return null;
+}
