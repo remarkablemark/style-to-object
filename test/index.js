9a2d46c17454df5affd6b65aed603677668b59e7
Mark <mark@remarkablemark.org>
2017-10-25 00:15:22 -0400

    chore(parser): move index.js -> lib/server-parser.js
    Export server-parser as the default parser in index (main) and
    update tests.

--- test/index.js
+++ test/index.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const assert = require('assert');
-const parser = require('../');
 const cases = require('./cases');
+const mainParser = require('../')
+const serverParser = require('../lib/server-parser');
 
-describe('parser', () => {
+describe('main parser', () => {
+  it('uses server parser by default', () => {
+    assert.equal(mainParser, serverParser);
+  });
+});
+
+describe('server parser', () => {
   cases.default.forEach(({ style, expected }) => {
     describe(`when style=\`${style}\``, () => {
       it(`returns ${JSON.stringify(expected)}`, () => {
-        assert.deepEqual(parser(style), expected);
+        assert.deepEqual(serverParser(style), expected);
       });
     });
   });
