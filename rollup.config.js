7b53a50c902216c7e08bcc7b90536b4d0ce13fa7
Mark <mark@remarkablemark.org>
2017-11-03 20:32:48 -0400

    chore(build): set up rollup and build scripts
    Save devDependencies:
    * rollup@0.51.5
    * rollup-plugin-commonjs@8.2.6
    * rollup-plugin-node-resolve@3.0.0
    * rollup-plugin-uglify@2.0.1
    * uglify-es@3.1.9

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
+
+const config = {
+  input: 'index.js',
+  output: {
+    format: 'umd',
+  },
+  name: 'StyleToObject',
+  plugins: [
+    resolve(),
+    commonjs(),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(uglify({}, minify));
+}
+
+export default config;
