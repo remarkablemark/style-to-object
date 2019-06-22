8d4c0048bd6c66e52036875bd78aaee2eeaf9afb
Mark <mark@remarkablemark.org>
2019-06-22 15:15:07 -0400

    test: organize and rename describe blocks

--- test/index.js
+++ test/index.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const assert = require('assert');
 const css = require('css');
 const cases = require('./cases');
-const parser = require('../');
+const parse = require('../');
 
 describe('parser', () => {
   cases.default.forEach(({ style, expected }) => {
@@ -11,47 +9,47 @@ describe('parser', () => {
     describe(`when style=\`${style}\``, () => {
       if (expected === Error) {
         it('throws error', () => {
-          assert.throws(() => parser(style), Error);
+          assert.throws(() => parse(style), Error);
         });
         return;
       }
 
       // normal case
       it(`returns ${JSON.stringify(expected)}`, () => {
-        assert.deepEqual(parser(style), expected);
+        assert.deepEqual(parse(style), expected);
       });
     });
   });
+});
 
-  describe('when iterator is passed', () => {
-    it('returns null', () => {
-      const style = cases.styles[0];
-      const iterator = () => {};
-      assert.equal(parser(style, iterator), null);
+describe('iterator', () => {
+  it('returns null', () => {
+    const style = 'color: #foo;';
+    const iterator = () => {};
+    assert.equal(parse(style, iterator), null);
+  });
+
+  it('invokes callback with arguments=[name, value, declaration]', () => {
+    const style = 'color: #f00;';
+    parse(style, (name, value, declaration) => {
+      assert.equal(name, 'color');
+      assert.equal(value, '#f00');
+      assert.deepEqual(
+        declaration,
+        css.parse(`p{${style}}`).stylesheet.rules[0].declarations[0]
+      );
     });
+  });
 
-    it('passes the arguments: name, value, declaration', () => {
-      const style = 'color: #f00;';
-      parser(style, (name, value, declaration) => {
-        assert.equal(name, 'color');
-        assert.equal(value, '#f00');
-        assert.deepEqual(
-          declaration,
-          css.parse(`p{${style}}`).stylesheet.rules[0].declarations[0]
-        );
-      });
-    });
-
-    it('parses comment', () => {
-      const style = '/* color: #f00; */';
-      parser(style, (name, value, declaration) => {
-        assert.equal(name, undefined);
-        assert.equal(value, undefined);
-        assert.deepEqual(
-          declaration,
-          css.parse(`p{${style}}`).stylesheet.rules[0].declarations[0]
-        );
-      });
+  it('parses comment', () => {
+    const style = '/* color: #f00; */';
+    parse(style, (name, value, declaration) => {
+      assert.equal(name, undefined);
+      assert.equal(value, undefined);
+      assert.deepEqual(
+        declaration,
+        css.parse(`p{${style}}`).stylesheet.rules[0].declarations[0]
+      );
     });
   });
 });
