c6e8a545182ad5526868cc50d128bcbb32936cd2
Mark <mark@remarkablemark.org>
2019-11-06 23:41:42 -0500

    fix(index): update return type of main function (remove `any`)

--- index.d.ts
+++ index.d.ts
@@ -4,13 +4,12 @@
  * @example
  * import StyleToObject from 'style-to-object';
  * StyleToObject('line-height: 42;');
- *
  */
-
 declare function StyleToObject(
   style: string,
   iterator?: StyleToObject.Iterator
-): { [name: string]: any } | null;
+): { [name: string]: string } | null;
+
 export = StyleToObject;
 
 declare namespace StyleToObject {
@@ -19,7 +18,7 @@ declare namespace StyleToObject {
     column: number;
   }
 
-  // declaration is object from module `inline-style-parser`
+  // declaration is an object from module `inline-style-parser`
   interface Declaration {
     type: string;
     property: string;
