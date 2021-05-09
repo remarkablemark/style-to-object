e0d06232c9882ebee94a41ba152edad9619b6406
v1rtl <pilll.pl22@gmail.com>
2021-05-09 23:09:42 +0300

    fix: test indentation and spelling error

--- test/esm.mjs
+++ test/esm.mjs
@@ -1,10 +1,14 @@
-import assert from 'assert'
-import StyleToObject from '../index.mjs'
+import assert from 'assert';
+import StyleToObject from '../index.mjs';
 
 describe('it works with ESM', () => {
-	it('imports the functiion without errors', () => {
-		assert.deepStrictEqual(StyleToObject('color: #C0FFEE; background: #BADA55;'), {
-			color: '#C0FFEE', background: '#BADA55'
-		})
-	})
-})
+  it('imports the function without errors', () => {
+    assert.deepStrictEqual(
+      StyleToObject('color: #C0FFEE; background: #BADA55;'),
+      {
+        color: '#C0FFEE',
+        background: '#BADA55'
+      }
+    );
+  });
+});
