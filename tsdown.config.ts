4142079dcc23d43bee6174bb580f8f06c702b5e4
Mark <mark@remarkablemark.org>
2026-06-14 01:11:19 -0400

    build: migrate rollup to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig([
+  // ESM build
+  {
+    entry: 'src/index.ts',
+    format: 'esm',
+    outDir: 'esm',
+    dts: true,
+    sourcemap: true,
+    external: ['inline-style-parser'],
+    outExtensions() {
+      return {
+        js: '.mjs',
+        dts: '.ts',
+      };
+    },
+  },
+
+  // CJS build
+  {
+    entry: 'src/index.ts',
+    format: 'cjs',
+    outDir: 'cjs',
+    dts: true,
+    sourcemap: true,
+    external: ['inline-style-parser'],
+    outExtensions() {
+      return {
+        js: '.js',
+        dts: '.ts',
+      };
+    },
+  },
+
+  // UMD build (unminified)
+  {
+    entry: 'src/index.ts',
+    format: 'umd',
+    outDir: 'dist',
+    globalName: 'StyleToObject',
+    sourcemap: true,
+    noExternal: ['inline-style-parser'],
+    outputOptions: {
+      entryFileNames: 'style-to-object.js',
+    },
+  },
+
+  // UMD build (minified)
+  {
+    entry: 'src/index.ts',
+    format: 'umd',
+    outDir: 'dist',
+    globalName: 'StyleToObject',
+    sourcemap: true,
+    minify: true,
+    noExternal: ['inline-style-parser'],
+    outputOptions: {
+      entryFileNames: 'style-to-object.min.js',
+    },
+  },
+]);
